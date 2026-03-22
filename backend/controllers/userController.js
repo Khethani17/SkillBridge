a71bb1ce6f4aac0eb7be52ab81e036d27b273e03
Khethani Achilles <khethani@Khethanis-MacBook-Air.local>
2026-03-22 17:41:48 +0200

    Backend auth system complete(register, login, JWT, middleware, profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,67 @@
-const createUser = (req, res) => {
-  const userData = req.body
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
 
-  res.status(201).json({
-    message: "User created successfully",
-    user: userData
-  })
+const registerUser = async (req, res) => {
+  try {
+    const { firstName, lastName, username, email, password, dateOfBirth } =
+      req.body;
+
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "A user already exists" });
+    }
+    const user = new User({
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+      dateOfBirth,
+    });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(201).json({
+      message: "User has been created",
+      token,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).json({
+    message: "Login successful",
+    token,
+    user,
+})
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getProfile = async (req, res) => {
+    res.status(200).json({ user: req.user })
 }
 
-module.exports = {
-  createUser
-}
+module.exports = { registerUser, loginUser, getProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const { registerUser, loginUser, getProfile } = require("../controllers/userController")
+const protect = require("../middleware/authMiddleware")
 
-const { createUser } = require("../controllers/userController")
 
-router.post("/users", createUser)
+router.post("/users", registerUser)
+router.post("/users/login", loginUser)
+router.get("/users/profile", protect, getProfile)
 
 module.exports = router
